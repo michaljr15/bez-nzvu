bcafa2a0966548e5768d77e69091aaf54ca25b2b
michaljr15 <132154927+michaljr15@users.noreply.github.com>
2023-04-29 18:06:29 +0200

    Počiatočné súbory pre projekt MakeCode

---
+++ main.ts
@@ -0,0 +1,14 @@
+basic.showIcon(IconNames.Chessboard)
+basic.showLeds(`
+    . . # . .
+    # . # . #
+    . # # # .
+    . # . # .
+    # # . # #
+    `)
+basic.pause(2000)
+basic.showIcon(IconNames.Diamond)
+basic.showString("friends")
+basic.forever(function () {
+	
+})
